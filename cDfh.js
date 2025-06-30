0e2afd2e02c603dd083962169699771d7157f8f6
olm-2025 <2024ionos@posteo.de>
2025-06-30 21:52:10 +0200

    Add files via upload

--- cDfh.js
+++ cDfh.js
@@ -1,22 +1,10 @@
 class cDfh{
 
     static init(oIn){
-
-        if(1)console.log([
-            "init...",
-            //oIn,
-            //oIn.page,
-            oIn.page.fileSlug,
-            //$(".rowbox.navi a"),
-            //$(`.rowbox.navi a[data-navi-val="${oIn.page.fileSlug}"]`),
-        ]);
-
+        //console.log(["init...",oIn.page.fileSlug]);
         $(".rowbox.hd").off().on("click",() => { this.hdlHdClick(); });
-
         $(`.rowbox.navi a[data-navi-val="${oIn.page.fileSlug}"]`).addClass("active");
-
-    }//init
-
+    }
 
     static hdlHdClick()  { location.href = '/?timestamp=123456'; }
 
